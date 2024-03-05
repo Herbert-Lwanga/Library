9bc0efcc4ce630866a422b6527d0499fce2bc084
Herbert-Lwanga <herbolwanga@gmail.com>
2024-03-05 15:04:50 +0300

    Using class instead of a factory function in creating a new Book and cleaning up the commented code left behind also cleaning up some unused code within the index.html file

--- script.js
+++ script.js
@@ -4,15 +4,13 @@ const addButton = document.querySelector(".add-book");
 const addToLibrary = document.querySelector(".add-to-library");
 const container2 = document.querySelector(".container-2");
 
-function Book(title, author, pages, status) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.status = status;
-    this.info = function(){
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.status} `
+class Book{ 
+    constructor(title,author,pages,status){
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.status = status;
     }
-
 }
 
 const svgCode = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="1em" height="1em"><title>trash-can-outline</title><path d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M7,6H17V19H7V6M9,8V17H11V8H9M13,8V17H15V8H13Z" /></svg>';
